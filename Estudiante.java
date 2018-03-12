35b3d804b5db19b64579b0efa5e1dfd747672d12
millercastillo <millercastillo@unicauca.edu.co>
2018-03-12 10:05:12 -0500

    borrado

--- Estudiante.java
+++ Estudiante.java
@@ -13,9 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 /**
  *
