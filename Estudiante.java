53b0038843a581b96f2d15ef6fedc888c466d5eb
jose tobar <37028397+josetobar26@users.noreply.github.com>
2018-03-12 10:09:17 -0500

    subir

--- Estudiante.java
+++ Estudiante.java
@@ -16,7 +16,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
+daskdjaskdjklasjkldasjkdjklasjkljkl
 /**
  *
  * @author jose
